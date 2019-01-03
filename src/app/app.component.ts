a5aa7ebeee3b7de3e1240bfa79454c44edf64a8b
Alex <sanekwwe@yandex.ua>
2019-01-03 11:48:59 +0200

    logger stub added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { LoggerService } from 'src/common/services/logger.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [LoggerService]
 })
 export class AppComponent {
   title = 'geo-stat';
+
+  constructor(private loggerService: LoggerService) { 
+    this.loggerService.log("App component created");
+  }
 }
